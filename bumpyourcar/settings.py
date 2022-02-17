1c402e37678f6e4b1192de66441d87b4a60a201c
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-17 11:40:52 +0700

    🚗 Now Siraphop want to deploy this to the server

--- bumpyourcar/settings.py
+++ bumpyourcar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default="an_awesome_secret_key_here")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
