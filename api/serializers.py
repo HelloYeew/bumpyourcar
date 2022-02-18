c388b34bc64385cb5c17c7602a50e45b9c7449f8
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-18 16:42:51 +0700

    🚗 Siraphop think that we must remove email from API

--- api/serializers.py
+++ api/serializers.py
@@ -19,7 +19,7 @@ class CarStatusSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name']
 
 
 class CarFullSerializer(serializers.ModelSerializer):
